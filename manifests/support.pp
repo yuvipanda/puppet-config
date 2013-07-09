c80a380ae7a8bc921aa21ebc5f11fa7df78511ba
YuviPanda <yuvipanda@gmail.com>
2013-07-09 22:08:27 +0530

    Initial commit.

---
+++ manifests/support.pp
@@ -0,0 +1,5 @@
+class support {
+	package { 'git':
+		ensure => 'present'
+	}
+}
---
+++ manifests/users.pp
@@ -0,0 +1,7 @@
+class users {
+    account{ 'yuvipanda':
+        groups => ['sudo'],
+        ssh_key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEArG2Z9FWP0Tp0QAVUPBPjY+PauM/07Ohn7qKrEjgR5upGKWRdYKiqnQH/kV7rbEJx5CSFVNxZ7CgerksBhkobfmn4JfYwDz6fa2JCcuASMBFLw6n7JbCdhH1IuIKjR3+G8rtPpq+AUsIIW3IKb/SqBtQTH23VSAb1POFo00MbiBZksd5sUdwbwZRuXicjr9FuGaEV3V/MkeW3mSC3u4P+slFfkgQujscFQ+gH6fQQiv2HTPqvPh56F/h4pVFqQcgK1bwSrbDV9UoMUPeZWJ2++A/vVBrut4kNLXWi6mHZ5SuyBBf+7wfnmpggTva6Fp3VSOa52GfSSK4Cc6TAKg/STQ==',
+        comment => 'YuviPanda, master of the servers (he wishes!)'
+    }
+}
