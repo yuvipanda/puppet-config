16e5df631b60a2021ac92616a925837f43b238d4
YuviPanda <yuvipanda@gmail.com>
2013-07-16 02:42:40 +0530

    Add empty group

--- manifests/users.pp
+++ manifests/users.pp
@@ -9,6 +9,7 @@ class users {
 
     localuser { 'sucheta':
         name    => 'sucheta',
+        groups  => [],
         ssh_key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDPYboGNFkrXFXLlmtWLmF/teGQDSahuhcghlPrV2DX5JgRsqWBiSXKQPnMcmUi39MJ7hjIAuHdZhb4yHpx/JIgO4ILFcNv/fkmcmrPj1vfHZbgn4SVFfR4m/fJZTFZs9Wz5CJ9mbmq5TglBZtMR0Mu9nI2BdpLoAMCo98AeZh5oHGzhIgarKtdx8q+JmE0vMArv3ii12qh06KkPyErtiocbOfQ6sSaX9aQm9fBq/4EXgK9J+HmxcWCfWtu6PeEUwiYF9bupZ9Q6WLyEXcsIY2jbCTTwqJoqDuijlqOD/zjIwFa1oVFMQ/dmJWC5k6d/aVrWL+qYg6zxD+eXhb0nvO7',
         comment => 'she is nice'
     }
