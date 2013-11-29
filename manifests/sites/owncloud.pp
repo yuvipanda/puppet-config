c01d3c41f53f013f64eb0c48e1257d1a9ef56403
Yuvi Panda <yuvipanda@gmail.com>
2013-11-30 03:07:27 +0530

    Ensure that owncloud folder is created first

--- manifests/sites/owncloud.pp
+++ manifests/sites/owncloud.pp
@@ -1,10 +1,17 @@
 class sites::owncloud ( $domain ) {
+    file { '/var/www/owncloud':
+        ensure => 'directory',
+        owner => 'www-data',
+        group => 'www-data'
+    }
+        
     apache::vhost { $domain:
         port => 9099,
         docroot => "/var/www/owncloud/${domain}",
         docroot_owner => 'www-data',
         docroot_group => 'www-data',
-        override => ['All']
+        override => ['All'],
+        require => File['/var/www/owncloud']
     }
 
     nginx::resource::upstream { "${host}-nginx-upstream":
