ada18cbcf6c191131a2d8017af1983a7de56b3cf
Yuvi Panda <yuvipanda@gmail.com>
2013-11-30 03:40:56 +0530

    Get rid of owncloud and related PHP things

--- manifests/servers.pp
+++ manifests/servers.pp
@@ -3,25 +3,7 @@ import "sites/*.pp"
 class servers {
     class { 'nginx': }
 
-    class { 'apache': 
-        default_mods => false,
-        default_vhost => false,
-        mpm_module => 'prefork'
-    }
-
-    class { 'apache::mod::php':
-    }
-
-    apache::mod { 'rewrite':
-    }
-
-    class { 'mysql::server':
-    }
-
     # Configuration for individual sites
     include sites::omgwtf
     include sites::losermatrimony
-    class { 'sites::owncloud':
-        domain => 'cloud.yuvi.in'
-    }
 }
