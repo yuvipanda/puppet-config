715870bcfbe7064b0b55d7057296e66dfbabd907
Yuvi Panda <yuvipanda@gmail.com>
2013-11-30 01:46:48 +0530

    Change my key to new one

--- manifests/users.pp
+++ manifests/users.pp
@@ -3,8 +3,8 @@ class users {
     localuser { 'yuvipanda':
         name    => 'yuvipanda',
         groups  => ['sudo'],
-        ssh_key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEArG2Z9FWP0Tp0QAVUPBPjY+PauM/07Ohn7qKrEjgR5upGKWRdYKiqnQH/kV7rbEJx5CSFVNxZ7CgerksBhkobfmn4JfYwDz6fa2JCcuASMBFLw6n7JbCdhH1IuIKjR3+G8rtPpq+AUsIIW3IKb/SqBtQTH23VSAb1POFo00MbiBZksd5sUdwbwZRuXicjr9FuGaEV3V/MkeW3mSC3u4P+slFfkgQujscFQ+gH6fQQiv2HTPqvPh56F/h4pVFqQcgK1bwSrbDV9UoMUPeZWJ2++A/vVBrut4kNLXWi6mHZ5SuyBBf+7wfnmpggTva6Fp3VSOa52GfSSK4Cc6TAKg/STQ==',
-        comment => 'YuviPanda, master of the servers (he wishes!)'
+        ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDrR80D68OpttnWGCY49ImSHPvzdCah1NckZv2U/y3Trmjp2EEeIohL8IG3iSjEnB1JirkPe1/0mHkbm0bfxqp17fHWy1g94Teb118woWt69A6pDJvnzp20faFRxOIl7UJjZ+n/Q4HSg7YToYW4hIM/I5KRKnEzi102aFHgV0cGGbxONPn0MiiEXkovlQ59gcNPWO/Wqe7gAcMIjoMvAcaumemUC5nXz8CIiFJLEfVVJ251c4q6C8TUJsxfbxkeN69AT/YqOiAO10eCbXgh0BNDactXy9oVx6u38E271KshW+ScJ671VuBQdTvv2DhrdFj2GsdzqlVYHQI9ny2JgtMX ypanda@Yuvi-Panda.local',
+        comment => 'YuviPanda'
     }
 
     localuser { 'sucheta':
